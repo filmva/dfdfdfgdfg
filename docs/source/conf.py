4ccef9142dc7b3c002f3d9b81daa849889a4dfe4
filmva <92706311+filmva@users.noreply.github.com>
2021-10-19 20:16:25 +0330

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'dfdfdfgdfg'
+copyright = '2021, dfdfdfgdfg'
+author = 'dfdfdfgdfg'
 
 release = '0.1'
 version = '0.1.0'
